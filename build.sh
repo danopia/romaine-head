33ce0fafd6b0862368035f8f515c1655124429df
Daniel Lamando <dan@danopia.net>
2015-08-20 23:00:35 -0700

    Generify shell a little

--- build.sh
+++ build.sh
@@ -2,23 +2,23 @@
 set -e # stop on error
 
 # Copyright Daniel Lamando 2015
-# Builds and packages a go program for dev-mode chromebooks running Crouton
+# Compiles a binary program for dev-mode chromebooks running Crouton
 # Outputted file can be easily downloaded (to ~/Downloads by Chrome)
 # The program can be launched from the chromeos shell as well as from a chroot:
 # $ sh ~/Downloads/romaine-head.run -- [options]
-# When launched on the chromeos side, uses root to install to $PATH.
+# When launched on the chromeos side, uses root/sudo to install to $PATH.
 
 # config for the entire script
 APP_NAME=romaine-head
-GO=/usr/local/go/bin/go
-GO_PACKAGE=github.com/danopia/$APP_NAME
+COMPILER="/usr/local/go/bin/go build"
+SOURCE=github.com/danopia/$APP_NAME
 TEMP=/tmp/$APP_NAME
 OUTPUT_PATH=~/Downloads/$APP_NAME.run
 INSTALL_PATH=/usr/local/bin/$APP_NAME
 
 # build binary
 mkdir -p $TEMP
-$GO build -o $TEMP/$APP_NAME $GO_PACKAGE
+$COMPILER -o $TEMP/$APP_NAME $SOURCE
 
 # add an install/launch script
 cat << EOF > $TEMP/run
